cb5077b38ee2047ba7efbc21d73b65eed8220010
Alex <Alex@gmail.com>
2022-09-01 12:31:54 +0430

    Change color of left borders after update and make delete

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{complete: prj.complete}">
     <div class="actions">
       <h3 @click="handleDetails">{{ prj.title }}</h3>
       <div class="icons">
         <span class="material-icons">edit</span>
-        <span class="material-icons">done</span>
+        <span class="material-icons" @click="changeComplete">done</span>
         <span class="material-icons" @click="deleteProject">delete</span>
       </div>
     </div>
@@ -20,7 +20,7 @@ export default {
     data() {
       return {
         showDetails: false,
-        uri: 'http://localhost:3000/projects' + this.prj.id  
+        uri: 'http://localhost:3000/projects/' + this.prj.id  
       }
     },
     methods: {
@@ -30,6 +30,15 @@ export default {
         deleteProject() {
             fetch(this.uri, {method: 'DELETE'})
                 .then(() => this.$emit('delete', this.prj.id))
+                .catch(err => console.log(err.message));
+        },
+        changeComplete() {
+            fetch(this.uri, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({complete: !this.prj.complete})
+            })
+                .then(() => this.$emit('complete', this.prj.id))
                 .catch(err => console.log(err.message));
         }
     }
@@ -61,5 +70,8 @@ h3 {
 }
 .material-icons:hover {
     color: #777;
+}
+.project.complete {
+    border-left: 4px solid #00ce89;
 }
 </style>
